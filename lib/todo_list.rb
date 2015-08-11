ba03f037496d766cdbbe38af34ced37e545f9182
Dillon Kearns <dillon@dillonkearns.com>
2015-08-11 16:00:43 -0700

    Remove some code so there's more to work on!

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -5,12 +5,7 @@ class TodoList
   attr_accessor :items
 
   def initialize
-    @items = []
-  end
-
-  def add(description)
-    new_item = TodoItem.new(description)
-    items << new_item
+    @items = nil
   end
 
 end
